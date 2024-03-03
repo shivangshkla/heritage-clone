61c11660e5c93520d9dc6df29435bc3325795e79
Shivang Shukla <shuklashivang036@gmail.com>
2024-03-03 14:25:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMenu() {
+    document.getElementById("nav-menu").classList.toggle("show");
+  }
+function phoneMenu() {
+    document.getElementById("nav-links").classList.toggle("phoneshow");
+ }
